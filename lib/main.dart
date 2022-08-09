188f349661543b5563d70c3e6503433ba4f0e832
LeFellowHuman <highgroundgamedev@gmail.com>
2022-08-09 22:00:09 +0300

    red button

--- lib/main.dart
+++ lib/main.dart
@@ -45,27 +45,48 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Align(
         alignment: Alignment.center,
-        child: Column(
-          children: [
-            Container(
-              child: Text('a Container'),
-              decoration: BoxDecoration(
-                color: Colors.green,
-                border: Border.all(width: 3, color: Colors.blue, strokeAlign: StrokeAlign.outside),
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black,
-                    offset: Offset(10, 10),
-                    blurRadius: 30,
-                  )
-                ],
-              ),
-              margin: EdgeInsets.all(20),
-              padding: EdgeInsets.only(top: 10, right: 20, bottom: 30, left: 40),
+        // child: Column(
+        //   children: [
+        //     Container(
+        //       child: Text('a Container'),
+        //       decoration: BoxDecoration(
+        //         color: Colors.green,
+        //         border: Border.all(width: 3, color: Colors.blue, strokeAlign: StrokeAlign.outside),
+        //         borderRadius: BorderRadius.all(Radius.circular(20)),
+        //         boxShadow: [
+        //           BoxShadow(
+        //             color: Colors.black,
+        //             offset: Offset(10, 10),
+        //             blurRadius: 30,
+        //           )
+        //         ],
+        //       ),
+        //       margin: EdgeInsets.all(20),
+        //       padding: EdgeInsets.only(top: 10, right: 20, bottom: 30, left: 40),
+        //     ),
+        //     RedContainer(),
+        //   ],
+        // ),
+        child: ElevatedButton.icon(
+          onPressed: () {},
+          icon: const Icon(Icons.abc),
+          label: const Text(
+            'a button',
+            style: TextStyle(
+              fontSize: 25,
+              fontStyle: FontStyle.italic,
+              letterSpacing: 10,
+              color: Colors.black,
             ),
-            RedContainer(),
-          ],
+          ),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(40),
+            ),
+            minimumSize: const Size(400, 50),
+            shadowColor: Colors.green,
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
